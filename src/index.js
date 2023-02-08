108fa3c03cc1a7db83dff3d98dda997d587f0628
Donald Akite <donaldakite27@gmail.com>
2023-02-08 23:13:23 +0000

    Remove unused code

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import { Provider } from 'react-redux';
-// import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-// import store from './redux/ConfigureStore';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
