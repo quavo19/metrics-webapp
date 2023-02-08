8feea0c8a135578fe287a72ad7d97152c42ed411
Donald Akite <donaldakite27@gmail.com>
2023-02-08 23:12:09 +0000

    Add test for navigation

---
+++ src/components/Navbar.test.js
@@ -0,0 +1,16 @@
+import renderer from 'react-test-renderer';
+import '@testing-library/jest-dom';
+import React from 'react';
+import Navbar from './components/Navbar';
+
+describe('test render', () => {
+  test('Header should match snapshoot', () => {
+    const tree = renderer.create(
+      <React.StrictMode>
+        <Navbar name="Home" returnpage={false} />
+      </React.StrictMode>,
+    ).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
