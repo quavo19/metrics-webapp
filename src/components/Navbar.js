8642729dce60677d123676480617848efd2e5aa0
Donald Akite <donaldakite27@gmail.com>
2023-02-08 14:34:52 +0000

    Add navigation for page

---
+++ src/components/Navbar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Row } from 'react-bootstrap';
+import { AiFillAudio } from 'react-icons/ai';
+import { IoChevronBackOutline } from 'react-icons/io5';
+import { IoMdSettings } from 'react-icons/io';
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => (
+  <div>
+    <NavLink to="/">
+      <IoChevronBackOutline className="iconBack" />
+    </NavLink>
+    <div>
+      <h3>Most views</h3>
+    </div>
+    <Row>
+      <div>
+        <AiFillAudio size={25} />
+      </div>
+      <div>
+        <IoMdSettings size={25} />
+      </div>
+    </Row>
+    <h4>Stats By Country</h4>
+  </div>
+);
+
+export default Navbar;
