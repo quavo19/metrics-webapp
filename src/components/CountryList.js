5a02e30b9f03b9ddf62d67ea17755cc626f7a40c
Donald Akite <donaldakite27@gmail.com>
2023-02-08 14:32:09 +0000

    Add function to Load every list country

---
+++ src/components/CountryList.js
@@ -0,0 +1,35 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { FetchCountries } from '../redux/countries/Countries';
+import './countryList.module.css';
+import Country from './Countries';
+
+const CountryList = () => {
+  const dispatch = useDispatch();
+  const listCountries = useSelector((state) => state.CountryReducer);
+  useEffect(() => {
+    if (listCountries.length === 0) {
+      dispatch(FetchCountries());
+    }
+  }, [dispatch, listCountries]);
+  return (
+    <>
+      <div className="Country-list">
+        <ul>
+          {listCountries.Countrieslist ? listCountries.Countrieslist.map((country) => (
+            <li key={country.numericCode}>
+              <Country
+                name={country.name}
+                flag={country.flag}
+                population={country.population}
+                numericCode={country.numericCode}
+              />
+            </li>
+          )) : <>Data Loading Please wait (:</> }
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default CountryList;
